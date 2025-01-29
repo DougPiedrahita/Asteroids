53ce288cdcebf3bf94c4cc38e613444e2cd1cf29
DougPiedrahita <36740336+DougPiedrahita@users.noreply.github.com>
2025-01-29 00:04:00 -0500

    update functionality

--- main.py
+++ main.py
@@ -2,15 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import *
-import circleshape
+from asteroidfield import *
+from asteroid import *
+from shot import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawables)
+    Asteroid.containers = (asteroids, updatable, drawables)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawables)
+    asteroid_field = AsteroidField()
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -19,10 +34,23 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over")
+                sys.exit()
+
+        for shot in shots:
+            if asteroid.collides_with(shot):
+                shot.kill()
+                asteroid.split()
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawables:
+            obj.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
